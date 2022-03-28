95300db18ceb6dc15fb4f8eba4963f9f549673ee
anannanur <anannaanu974@gmail.com>
2022-03-28 21:14:21 +0600

    creating navbar

---
+++ src/Components/Link/Link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Link = (props) => {
+    const {name,link} = props.route;
+    return (
+        <div>
+            <li><a href={link}>{name}</a></li>
+        </div>
+    );
+};
+
+export default Link;
